cc9450ca1ac595d680365a111551dcf1c1ea3255
domi555 <66876360+domi555@users.noreply.github.com>
2022-01-27 14:23:07 +0100

    Update

---
+++ note/public/service-worker.js
@@ -0,0 +1,9 @@
+/* global workbox */
+
+if (workbox) {
+  console.log(`Workbox is loaded`);
+  
+  workbox.precaching.precacheAndRoute(self.__precacheManifest);
+} else {
+  console.log(`Workbox didn't load`);
+}
---
+++ note/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar app color="dark" dark>
+      <img src="./assets/logo.png" style="width: 30px" />
+      <v-toolbar-title class="ms-2 font-weight-bold">NOTE</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn to="/" class="me-2" active-class="red darken-3">Home</v-btn>
+      <v-btn to="/notes" active-class="red darken-3">Notes</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    serverAddress: process.env.VUE_APP_SERVER,
+  }),
+};
+</script>
